950f7e00af9b26fb714e691be0530a58e2b80fbb
unknown <nosracer_gt@sbcglobal.net>
2019-07-31 22:51:18 -0500

    Added two packes to get svg icons on the site which will link to my personal accoutns

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,38 +1,41 @@
 import React, { Component } from "react";
 import "./Footer.css";
+import simpleIcons from "simple-icons";
+import parse from "html-react-parser";
 
 class Footer extends Component {
   render() {
     return (
       <div className="footer">
         <div className="footer__contact">
-          <p className="footer__heading">Contact Me</p>
-          <p className="footer__text">
+          <p className="footer__contact__heading">Contact Me</p>
+          <p className="footer__contact__text">
             If you are experience problems with the webpage, desktop app, or
-            have any concerns or suggestions about MTGA Pathway, please reach
-            out to me at <span>markalaniz000@gmail.com</span>
+            have any concerns or suggestions about MTGA Pathway, you may find me
+            at
           </p>
-          <p>You may also find me at</p>
+          {parse(simpleIcons.get("github").svg)}
         </div>
         <div classname="footer__support">
-            <p>I hope you enjoy the project!</p>
+          <p>I hope you enjoy the project!</p>
           <p>
             If you enjoy what I've created please consider supporting my efforts
-            to continue creating tools that people with similar interests make good use of.
+            to continue creating tools that people with similar interests make
+            good use of.
           </p>
           <a
-          href="https://ko-fi.com/T6T110922"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            height="36"
-            style={{ border: "0px", height: "36px" }}
-            src="https://az743702.vo.msecnd.net/cdn/kofi1.png?v=2"
-            border="0"
-            alt="Buy Me a Coffee at ko-fi.com"
-          />
-        </a>
+            href="https://ko-fi.com/T6T110922"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              height="36"
+              style={{ border: "0px", height: "36px" }}
+              src="https://az743702.vo.msecnd.net/cdn/kofi1.png?v=2"
+              border="0"
+              alt="Buy Me a Coffee at ko-fi.com"
+            />
+          </a>
         </div>
       </div>
     );
