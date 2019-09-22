c1e9975500efe1f43613778ee3c66d8a78881c1c
unknown <nosracer_gt@sbcglobal.net>
2019-09-22 17:38:04 -0500

    Added some styles to the 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,29 @@
 import React from "react"
-
+import styled from "styled-components"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import logo from "../images/app-icon.png"
 
+const FourOhFourWrapper = styled.div`
+  width: 100%;
+  padding-top: 80px;
+`
+const FourOhFourMsg = styled.div`
+  padding: 10% 0;
+  text-align: center;
+  color: black;
+`
 const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+  <FourOhFourWrapper>
+    <Layout>
+      <SEO title="404: Not found" />
+      <FourOhFourMsg>
+        <img src={logo} />
+        <h1>NOT FOUND</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      </FourOhFourMsg>
+    </Layout>
+  </FourOhFourWrapper>
 )
 
 export default NotFoundPage
